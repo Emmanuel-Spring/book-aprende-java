f70b38834d91ec89f8b3b361ada3da40da3002fc
Emmanuel Nieto <emmanuel.nieto@tisal.cl>
2022-06-28 20:07:43 -0400

    primer commit, capítulo 4

--- src/main/java/cl/springs/book/chapter04/ejercicios/Ejercicio01.java
+++ src/main/java/cl/springs/book/chapter04/ejercicios/Ejercicio01.java
@@ -1,7 +1,7 @@
 package cl.springs.book.chapter04.ejercicios;
 
 public class Ejercicio01 {
+    public static void main(String[] args) {
 
-
-
+    }
 }
--- src/main/java/cl/springs/book/chapter04/ejercicios/Ejercicio29.java
+++ src/main/java/cl/springs/book/chapter04/ejercicios/Ejercicio29.java
@@ -1,7 +1,9 @@
 package cl.springs.book.chapter04.ejercicios;
 
 public class Ejercicio29 {
+    public static void main(String[] args) {
 
 
 
+    }
 }
